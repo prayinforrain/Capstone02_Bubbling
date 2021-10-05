fa090719523efdc42f2f348df91ec62e4affa4e8
PrayinForRain <dldnwo99@naver.com>
2021-10-05 12:53:52 +0900

    211005 ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ
    네비게이(gay아님)터 추가

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import LoginComponent from './componenets/login/LoginComponent';
 import Maptest from './componenets/Maptest';
 import TopFrame from './componenets/main/TopFrame';
 import { auth } from './firebaseConfig';
 import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const StackNav = createNativeStackNavigator();
+const TabNav = createBottomTabNavigator();
+
 
 export default function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [userToken, setUserToken] = useState(null);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if(user) {
@@ -23,21 +39,43 @@ export default function App() {
     });
   }, [])
 
-  return (
-    <View style={styles.container}>
+  /*
+    <SafeAreaView style={styles.container}>
       {isLoggedIn ?
-      <View>
-        <View style={styles.topFrame}>
-          <TopFrame/>
-        </View>
-        <View style={styles.mainContent}>
-          <Maptest/>
-        </View>
-        <View style={styles.bottomFrame}>
-        </View>
-      </View> :
-      <LoginComponent/>}
-    </View>
+        <View>
+          <View style={styles.topFrame}>
+            <TopFrame/>
+          </View>
+          <View style={styles.mainContent}>
+            <Maptest/>
+          </View>
+          <View style={styles.bottomFrame}>
+          </View>
+        </View> :
+        <LoginComponent/>
+      }
+    </SafeAreaView>
+  */
+  return (
+      <NavigationContainer>
+        <TabNav.Navigator
+        initialRouteName="Home"
+        screenOptions= {{
+          title: "AppName",
+          headerLeft: function() {
+            return (
+              <Text>Left</Text>
+            )
+          },
+          headerRight: function() {
+            return(
+              <Text>Right</Text>
+            )
+          }
+        }}>
+          <TabNav.Screen name="Home" component={Maptest}/>
+        </TabNav.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -52,8 +90,8 @@ const styles = StyleSheet.create({
     flex: 10
   },
   topFrame: {
-    paddingTop: 60,
-    flex: 2
+    flex: 2,
+    backgroundColor: "blue"
   },
   bottomFrame: {
     flex: 2
